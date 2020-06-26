4032d2f388b968731627251deb980d7900c1a414
Erik Björn <erik.g.bjoern@gmail.com>
2020-06-26 13:11:27 +0200

    adding create action for comments, happy path

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,4 +1,8 @@
 class Api::CommentsController < ApplicationController
   def create
+    comment = Comment.create(content: params[:content], article_id: params[:article_id])
+    if comment.persisted?
+      render json: { message: "Your comment was succesfully posted" }
+    end
   end
 end
